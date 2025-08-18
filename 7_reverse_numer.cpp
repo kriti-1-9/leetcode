df16d419a3bd0161e88c172ffcbf7bab367d4bd0
kriti-1-9 <dwivedikriti700@gmail.com>
2025-08-18 21:05:56 +0530

    Added 74: search in 2D matrix

--- 7_reverse_numer.cpp
+++ 7_reverse_numer.cpp
@@ -3,7 +3,7 @@ public:
     int reverse(int x) {
         int revNum = 0;
         while(x != 0) {
-            int dig = x%10;
+            int dig = x % 10;
              if(revNum > INT_MAX/10 || revNum < INT_MIN/10) {
               return 0;
             }
