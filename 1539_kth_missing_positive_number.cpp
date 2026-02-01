8b7518614787cf36de89aad302f4347e4e643a38
kriti-1-9 <dwivedikriti700@gmail.com>
2026-02-01 19:04:58 +0530

    Added ques 1539: Kth missing positive number

---
+++ 1539_kth_missing_positive_number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int low = 0, high = arr.size() - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int missing = arr[mid] - (mid + 1);
+            
+            if (missing < k) {
+                low = mid + 1; // Need to look further right for more missing numbers
+            } else {
+                high = mid - 1; // Too many missing numbers, look left
+            }
+        }
+        
+        // The formula for the result is: high + 1 + k
+        // Or more simply: low + k
+        return low + k;
+    }
+};
