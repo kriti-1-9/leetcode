1b5799c19d8e208a7864736d93adbfefb33ef537
kriti-1-9 <dwivedikriti700@gmail.com>
2026-02-03 21:24:26 +0530

    Added ques 4: Median of 2 sorted arrays

--- 1539_kth_missing_positive_number.cpp
+++ 1539_kth_missing_positive_number.cpp
@@ -1,29 +1,21 @@
-// class Solution {
-// public:
-//     int findKthPositive(vector<int>& arr, int k) {
-//         int low = 0, high = arr.size() - 1;
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int low = 0, high = arr.size() - 1;
         
-//         while (low <= high) {
-//             int mid = low + (high - low) / 2;
-//             int missing = arr[mid] - (mid + 1);
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int missing = arr[mid] - (mid + 1);
             
-//             if (missing < k) {
-//                 low = mid + 1; // Need to look further right for more missing numbers
-//             } else {
-//                 high = mid - 1; // Too many missing numbers, look left
-//             }
-//         }
+            if (missing < k) {
+                low = mid + 1; // Need to look further right for more missing numbers
+            } else {
+                high = mid - 1; // Too many missing numbers, look left
+            }
+        }
         
-//         // The formula for the result is: high + 1 + k
-//         // Or more simply: low + k
-//         return low + k;
-//     }
-// };
-
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
-}
+        // The formula for the result is: high + 1 + k
+        // Or more simply: low + k
+        return low + k;
+    }
+};
